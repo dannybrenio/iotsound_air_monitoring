b85a88e45ed0491a47df3d6c76de24480724f168
Danny LNX <dannybrenio060@gmail.com>
2025-10-20 12:09:31 +0800

    pending data logic working

--- app/Http/Controllers/PendingHardwareDataController.php
+++ app/Http/Controllers/PendingHardwareDataController.php
@@ -10,12 +10,56 @@ class PendingHardwareDataController extends Controller
 
     public function index()
     {
-        //
+    
     }
 
-    public function store(Request $request)
+    public function store($pending_hardware_info, $pm2_5, $pm10, $co, $no2, $decibels, $realtime_stamp)
     {
-        //
+        try{
+        //$rawdata = $request->json()->all();
+
+        // $hardware_id = Hardware::where('hardware_info', $rawdata['hardware_info'])->value('hardware_id');
+        //  if(empty($hardware_id)){
+
+            
+        //     return response()->json(['success' => true, 
+        //     'hardware_info' => $rawdata['hardware_info'],
+        //     'message' => 'Hardware ID not found, added to pending data.'], 200);
+        //  }
+
+            // $pending_hardware_data = Pending_hardware_data::create([
+            //     'pending_hardware_info' => $rawdata['hardware_info'],
+            //     'pm2_5' => $rawdata['pm2_5'] ?? null,
+            //     'pm10' => $rawdata['pm10']?? null,
+            //     'co' => $rawdata['co']?? null,
+            //     'no2' => $rawdata['no2']?? null,
+            //     'decibels' => $rawdata['decibels']?? null,
+            //     'realtime_stamp' => $rawdata['realtime_stamp']?? null,
+            // ]);
+            
+
+            $pending_hardware_data = Pending_hardware_data::create([
+                'pending_hardware_info' => $pending_hardware_info,
+                'pm2_5' => $pm2_5 ?? null,
+                'pm10' => $pm10 ?? null,
+                'co' => $co ?? null,
+                'no2' => $no2 ?? null,
+                'decibels' => $decibels ?? null,
+                'realtime_stamp' => $realtime_stamp ?? null,
+            ]);
+
+            //pending data shouldnt alert users if the data is pending
+            // if($hardware_data){
+            //     $alertsController = new AlertsController();
+            //     $alertsController->store($rawdata['pm2_5'], $rawdata['pm10'], $rawdata['co'], $rawdata["no2"], $rawdata["decibels"]);
+            // }
+    } catch (Exception $e) {
+        return response()->json([
+            'error'   => 'Server Error',
+            'message' => $e->getMessage(),
+            'trace'   => $e->getTrace()
+        ], 500);
+    }
     }
 
 }
