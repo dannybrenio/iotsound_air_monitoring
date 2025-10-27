3a9b2eab2c8f447cfe3f0bd681f188b4d885603a
Danny LNX <dannybrenio060@gmail.com>
2025-10-27 17:22:13 +0800

    added missing parameter in history_blade

--- resources/views/admin/status/history_status.blade.php
+++ resources/views/admin/status/history_status.blade.php
@@ -12,6 +12,7 @@
             <th>History Id</th>
             <th>Hardware Info</th>
             <th>Sensor Type</th>
+            <th>Sensor Status</th>
             <th>Created At</th>
         </tr>
     @foreach($history_statuses as $history_status) 
@@ -19,6 +20,7 @@
     <td><p> {{$history_status->history_id}} </p></td>
     <td><p> {{$history_status->device_status?->hardware_info}} </p></td>
     <td><p> {{$history_status->sensor_type}} </p></td>
+     <td><p> {{$history_status->sensor_status}} </p></td>
     <td><p> {{$history_status->created_at}} </p></td>
                     </tr>
 @endforeach
